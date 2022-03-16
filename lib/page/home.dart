6ae79d41b006b3f3c7835a22ea288ffa7e9ee1c7
Sxit1234 <zhangyong@q-clouds.com>
2022-03-16 12:54:40 +0800

    master 创建首页

---
+++ lib/page/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
