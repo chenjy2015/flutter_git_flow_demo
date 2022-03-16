cc57bd7dbb1346c76d92e0a7c2c02b5d5ca83ef4
Sxit1234 <zhangyong@q-clouds.com>
2022-03-16 13:21:46 +0800

    create tab page

---
+++ lib/page/mine/mine.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class MinePage extends StatefulWidget {
+  const MinePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => MinePageState();
+}
+
+class MinePageState extends State<MinePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
